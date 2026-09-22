3ff9396df148f3c56ee614c0ef8abbc1657f1dd6
Leeviii-SH <leeviiizhang@gmail.com>
2026-09-22 11:53:36 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,11 +19,10 @@ export default {
     const proxies = [
       {
         prefix: '/api/hortor-ucenter',
-        target: 'https://comb-platform.hortorgames.com',
+        target: 'https://ucenter-app-server.hortorgames.com',
         headers: {
           'User-Agent': 'Mozilla/5.0 (Linux; Android 12; ALN-AL80 Build/HUAWEIALN-AL80; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.196 Mobile Safari/537.36',
-          'Accept': 'application/json',
-          'Host': 'ucenter-app-server.hortorgames.com',
+          'Accept': 'application/json',          
           'Content-Type': 'application/json; charset=utf-8'
         }
       },
